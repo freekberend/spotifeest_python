c10781e12f4e495e033d88dc0ae26656003ee94f
ngrev <n.greven23@gmail.com>
2022-12-09 11:39:21 +0100

    sql table

---
+++ createtables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE track(
+    track_id TEXT PRIMARY KEY NOT NULL,
+    track TEXT NOT NULL
+);
+
+CREATE TABLE artist(
+    artist_id TEXT PRIMARY KEY NOT NULL,
+    artist TEXT NOT NULL,
+    track_id TEXT NOT NULL,
+    FOREIGN KEY (track_id) REFERENCES track (track_id)
+);
+
+CREATE TABLE genre(
+    genre_id TEXT PRIMARY KEY NOT NULL,
+    genre TEXT NOT NULL,
+    artist_id TEXT NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES artist (artist_id)
+);
--- nathalie.py
+++ nathalie.py
@@ -1,12 +1,14 @@
 import get_recommendations
+import sqlite3
 
 def mehodenathalie1():
     return "dit is methode1"
 
 def party():
-    access_token = get_recommendations.get_access_token()
-    recommendations = get_recommendations.get_recommendations_on_spotify(access_token, limit=1, seed_genres='pop')
-    for artist in recommendations.get("tracks")[0].get("artists"):
-        print(artist['id'])
-    return recommendations.get("tracks")[0].get("artists")
-    return recommendations
+    recommendations = get_recommendations.get_genres_for_recommendation(limit=1, seed_genres='pop')
+    connection=sqlite3.connect(":memory:")
+    with open("createtables.sql") as f:
+        query = f.read()
+    cursor = connection.cursor()
+    cursor.executescript(query)
+    print(get_recommendations.get_genres_for_recommendation())
